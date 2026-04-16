8aaba3aa5e9cbdab406e405d41680f7211f6f559
asafox <asafox2086@gmail.com>
2026-04-16 19:18:08 +0800

    update find

--- user/find.c
+++ user/find.c
@@ -1,4 +1,3 @@
-/*
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
@@ -63,68 +62,5 @@ int main(int argc, char *argv[])
     }else{
         find(argv[1],argv[2]);
     }
-    exit(0);
-}
-*/
-
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
-#include "kernel/fs.h"
-
-
-void find_file(const char* dirpath, const char* filename) {
-    int fd = open(dirpath, 0);
-    if (fd < 0) {
-        fprintf(2, "can't open %s\n", dirpath);
-        exit(1);
-    }
-    
-    struct dirent de;
-    
-    char buf[512], *p;
-    if (strlen(dirpath) + 1 + DIRSIZ + 1 > sizeof(buf)) {
-        fprintf(2, "find: path too long\n");
-        exit(1);
-    }
-    strcpy(buf, dirpath);
-    p = buf + strlen(buf);
-    *p++ = '/';
-
-    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
-        if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) {
-            continue;
-        }
-
-        memmove(p, de.name, DIRSIZ);
-        p[DIRSIZ] = 0;
-        struct stat st;
-        if (stat(buf, &st) < 0) {
-            printf("cannot stat %s\n", buf);
-            continue;
-        }
-        switch (st.type) {
-            case T_DEVICE:
-            case T_FILE:
-                if (strcmp(de.name, filename) == 0) {
-                    printf("%s\n", buf);
-                }
-                break;
-            case T_DIR:
-                find_file(buf, filename);
-                break;
-        }
-    }
-    close(fd);
-}
-
-
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(2, "Usage: find directory filename\n");
-        exit(1);
-    }
-
-    find_file(argv[1], argv[2]);
     exit(0);
 }
--- user/xargs.c
+++ user/xargs.c
@@ -2,39 +2,49 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/param.h"
+
 int main(int argc, char *argv[])
 {
-    if(argc<2){
-        fprintf(2,"Usage : xargs <command> [args...]\n");
+    if(argc < 2){
+        fprintf(2, "Usage: xargs <command> [args...]\n");
         exit(0);
     }
-    char *command=argv[1];
+
+    char *command = argv[1];
     char *new_argv[MAXARG];
     char buf[512];
-    for (int i=1;i<argc;i++){
-        new_argv[i-1]=argv[i];
-    }
+
+    for(int i = 1; i < argc; i++)
+        new_argv[i - 1] = argv[i];
+
     while(1){
-        int p=0;
+        int p = 0;
         char c;
-        int n;
-        while((n=read(0,&c,1))>0){
-            if(c=='\n'){
+        int n = 0;
+
+        while((n = read(0, &c, 1)) > 0){
+            if(c == '\n'){
                 break;
             }
-            buf[p++]=c;
+            if(p + 1 < sizeof(buf))
+                buf[p++] = c;
         }
-        if(n<=0&&p==0) break;
-        buf[p++]='\0';
-        new_argv[argc-1]=buf;
-        new_argv[argc]=0;
-        if(fork()==0){
-            exec(command,new_argv);
-            fprintf(2,"exec failed\n");
+
+        if(n <= 0 && p == 0)
+            break;
+
+        buf[p] = '\0';
+        new_argv[argc - 1] = buf;
+        new_argv[argc] = 0;
+
+        if(fork() == 0){
+            exec(command, new_argv);
+            fprintf(2, "exec %s failed\n", command);
             exit(1);
-        }else{
+        } else {
             wait(0);
         }
     }
+
     exit(0);
 }
